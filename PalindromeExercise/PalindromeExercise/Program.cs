d2c9078c7fa78b8154894ac2ae0988c0bd2198bf
Kyle Cleary <kscleary11@gmail.com>
2024-07-15 14:42:10 -0600

    Exercise Complete

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -6,7 +6,8 @@ namespace PalindromeExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using PalindromeExercise;
+    
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTests
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("John", false)]
+        [InlineData("Jose", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+
+
+        }
+    }
+}
